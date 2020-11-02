f1da9e28f2a80fe2ddf299dd83814b4102a1fc9b
Jesse Hattabaugh <arkanciscan@gmail.com>
2020-11-02 10:34:29 -0800

    Same thing but lists all the files in a directory

--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,24 @@ export default function Home() {
   return (<div>
     <h1>readm.us</h1>
     <h2>the PWA that reads to you</h2>
-    <button onClick={readSomething}>Give me something to read</button>
+    <button onClick={showDirectoryPicker}>Give me something to read</button>
   </div>)
 }
 
-async function readSomething() {
-  const someFiles = await window.showOpenFilePicker({ multiple: true });
-  //const someFiles = await window.showOpenFilePicker();
-  for (const file of someFiles) {
-    console.log(file.kind, file.name);
+async function showDirectoryPicker() {
+  const dirHandle = await window.showDirectoryPicker();
+  for await (const entry of dirHandle.values()) {
+    console.dir(entry);
+    switch (entry.kind) {
+      case 'file':
+        console.log(`File named ${entry.name}`);
+        break;
+      case 'directory':
+        console.log(`Directory named ${entry.name}`);
+      break;
+      default:
+        console.log(entry);
+        break;
+    }
   }
-
 }
