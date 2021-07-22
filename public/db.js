c604bc967fb56b1c9c7843a13196c15133047c8b
Jesse Hattabaugh <arkanciscan@gmail.com>
2021-07-22 14:08:34 -0700

    distances

--- public/db.js
+++ public/db.js
@@ -4,4 +4,5 @@ export const db = new Dexie('Books');
 
 db.version(1).stores({
 	files: '++id, name',
+	distances: '++id, &[fileName+otherFileName], &[otherFileName+fileName]',
 });
--- public/load-files.js
+++ public/load-files.js
@@ -1,75 +1,101 @@
 import { db } from './db.js';
 import { html } from 'https://cdn.skypack.dev/lit-html';
 import { component, useState } from 'https://cdn.skypack.dev/haunted';
+import { distance as leven } from 'https://cdn.skypack.dev/fastest-levenshtein';
 
 export const LoadFiles = component(() => {
-	const [files, setFiles] = useState([]);
+	const [status, setStatus] = useState('');
+	const [duplicates, setDuplicates] = useState([]);
 
-	return html` ${!files.length
-		? html`<button
-					@click="${async () => {
-						console.info(`📚 loading books`);
-						const dirHandle = await window.showDirectoryPicker();
-						console.time('scanning');
-						await scanDir(dirHandle);
-						console.timeEnd('scanning');
-						const names = await db.files
-							.orderBy('name')
-							.uniqueKeys();
-						console.info(
-							`🧾 found ${names.length} normalized names`,
-						);
-						setFiles(names);
-					}}"
-				>
-					Load Files
-				</button>
-				<p>No files loaded yet</p>`
-		: html`<p>${files.length} files found!</p>
-				<ul>
-					${files.map(
-						(normalizedName) =>
-							html`<li>
-								<file-details
-									.name=${normalizedName}
-								></file-details>
-							</li>`,
-					)}
-				</ul>`}`;
+	async function onClick() {
+		const dirHandle = await window.showDirectoryPicker();
+		setStatus('scanning for files');
+		console.time('🔍 scanning for files');
+		await scanDir(dirHandle);
+		console.timeEnd('🔍 scanning for files');
+		setStatus('looking for duplicates');
+		const names = await db.files.orderBy('name').uniqueKeys();
+		const n = names.length;
+		console.info(`🧾 found ${n} unique file names`);
+		console.time('📏 calculating distances');
+		let toAdd = [];
+		for (let i = 0; i < n; i++) {
+			const fileName = names[i];
+			for (let j = i + 1; j < n; j++) {
+				const otherFileName = names[j];
+				const distance = leven(fileName, otherFileName);
+				if (distance < 10) {
+					toAdd.push({
+						fileName,
+						otherFileName,
+						distance,
+					});
+				}
+			}
+			//console.debug(`👬 compared all distances for ${fileName}`);
+			await db.distances.bulkAdd(toAdd);
+			toAdd = [];
+		}
+		console.timeEnd('📏 calculating distances');
+		const distances = await db.distances.orderBy('fileName').toArray();
+		const duplicates = distances.reduce((acc, cur, i) => {
+			const { fileName, otherFileName, distance } = cur;
+			acc[fileName] = [
+				...(acc[fileName] || []),
+				{ otherFileName, distance },
+			];
+			return acc;
+		}, []);
+		setDuplicates(duplicates);
+		setStatus('done');
+	}
+
+	return html` <h2>Status: ${status}</h2>
+		${!duplicates.length
+			? html`<button @click="${onClick}">Load Files</button>
+					<p>No files loaded yet</p>`
+			: html`<p>${duplicates.length} files found!</p>
+					<ul>
+						${duplicates.map(
+							({ fileName, otherFileName, distance }) =>
+								html`<li>
+									${distance} : ${fileName} - ${otherFileName}
+								</li>`,
+						)}
+					</ul>`}`;
 });
 
 async function scanDir(dirHandle) {
-	console.info(`📁 scanning directory for books`);
+	//console.debug(`📁 scanning directory for books`);
+	let toAdd = [];
 	for await (const entry of dirHandle.values()) {
 		if (entry.kind == 'directory') {
-			console.info(`📂 found a directory: ${entry.name}`);
+			//console.debug(`📂 found a directory: ${entry.name}`);
 			await scanDir(entry);
 		} else if (entry.name.endsWith('epub') || entry.name.endsWith('pdf')) {
-			console.info(`📕 found a book: ${entry.name}`);
-			const normalizedName = (
+			const name =
 				entry.name.substring(0, entry.name.lastIndexOf('.')) ||
-				entry.name
-			)
-				.toLowerCase()
-				.replaceAll(/[\W|_]+/g, '');
+				entry.name;
+			//console.debug(`📕 found a book: ${entry.name}`);
 			let fileData;
 			try {
 				const file = await entry.getFile();
-				const { name, type, lastModified, size } = file;
+				const { type, lastModified, size } = file;
 				fileData = {
-					fileName: name,
 					lastModified,
-					name: normalizedName,
+					name,
 					size,
 					type,
 				};
 			} catch (Error) {
 				console.warn(`${entry.name} - ${Error.message}`);
 				fileData = {
-					name: normalizedName,
+					name,
 				};
 			}
-			db.files.add(fileData);
+			toAdd.push(fileData);
 		}
 	}
+	db.files.bulkAdd(toAdd);
+	toAdd = [];
 }
